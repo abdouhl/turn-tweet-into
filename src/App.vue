74ff269a9279d097506078013ff92b5e14e5a8dc
abdou_hl <abderrahmane.36.hellal@gmail.com>
2022-11-22 19:04:59 +0100

    try fetch method

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,16 @@
 export default {
   data() {
     return {
-      count:0
+      count:0,
+      tweet_id:'1560712321860771840',
+      result:""
+    }
+  },
+  methods:{
+    getdata(){
+      fetch("https://cdn.syndication.twimg.com/tweet-result?id=1567531687130632199")
+        .then(res =>res.json())
+        .then(data =>this.result = data);
     }
   }
 }
@@ -10,12 +19,15 @@ export default {
 
 <template>
 <button @click="count++">count: {{ count }}</button>
+<button @click="getdata()">get data</button>
+<p >{{ result }}</p>
 </template>
 
 <style>
 button {
   font-weight:bold;
 }
+
 </style>
 
 
