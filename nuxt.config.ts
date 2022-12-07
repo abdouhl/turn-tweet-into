50d425e81eb9c482d942855375cee5999e9912a7
abdou_hl <abderrahmane.36.hellal@gmail.com>
2022-12-07 17:33:34 +0100

    site init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ["canvas", "image-data-uri", "node-canvas-with-twemoji-and-discord-emoji"],
   runtimeConfig: {
     apiUrl: process.env.API_URL
-  }
+  },
+  nitro: {
+    preset: "vercel",
+  },
 })
