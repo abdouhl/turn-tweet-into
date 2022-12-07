33e77a4d48ca4129e027329f4b34b775383d9b66
abdou_hl <abderrahmane.36.hellal@gmail.com>
2022-12-07 15:20:28 +0100

    site init

--- server/api/tweet/[tweet_id].js
+++ server/api/tweet/[tweet_id].js
@@ -1,7 +1,7 @@
 import { registerFont,createCanvas, Image } from 'canvas'
 import { fillTextWithTwemoji } from 'node-canvas-with-twemoji-and-discord-emoji';
-
 import ImgD from 'image-data-uri'
+
 export default defineEventHandler(async (event) => {
 const {tweet_id} = event.context.params
 const {name,username,profile_image_url_https,text,photos,tweet_text_dir}= await $fetch(useRuntimeConfig().apiUrl+tweet_id)
