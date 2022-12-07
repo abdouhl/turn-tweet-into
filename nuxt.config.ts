616cb1f823dd628993f55e8dd85ed8bdc347b762
abdou_hl <abderrahmane.36.hellal@gmail.com>
2022-12-07 17:57:22 +0100

    site init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["canvas@2.9.1", "image-data-uri", "node-canvas-with-twemoji-and-discord-emoji"],
   runtimeConfig: {
     apiUrl: process.env.API_URL
   },
