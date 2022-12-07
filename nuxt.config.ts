11ade8bfcef5aa13192fcac5b3333e5f615eae6d
abdou_hl <abderrahmane.36.hellal@gmail.com>
2022-12-07 15:56:10 +0100

    site init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  nitro: {
-    preset: "vercel",
-  },
   runtimeConfig: {
     apiUrl: process.env.API_URL
   }
