9078b7fe485cb971d654ab97fd90955387cb83f7
abdou_hl <abderrahmane.36.hellal@gmail.com>
2022-12-08 10:03:59 +0100

    init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+if (
+  process.env.LD_LIBRARY_PATH == null ||
+  !process.env.LD_LIBRARY_PATH.includes(
+    `${process.env.PWD}/node_modules/canvas/build/Release:`,
+  )
+) {
+  process.env.LD_LIBRARY_PATH = `${
+    process.env.PWD
+  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
+}
 export default defineNuxtConfig({
   runtimeConfig: {
     apiUrl: process.env.API_URL
