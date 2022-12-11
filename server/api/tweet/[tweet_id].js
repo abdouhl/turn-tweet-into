ca77f6c2bf39bcd29e1308df29156d32db5c8094
abdou_hl <abderrahmane.36.hellal@gmail.com>
2022-12-11 17:45:37 +0100

    fix font

--- server/api/tweet/[tweet_id].js
+++ server/api/tweet/[tweet_id].js
@@ -2,7 +2,7 @@ import { registerFont,createCanvas, Image } from 'canvas'
 import { fillTextWithTwemoji } from 'node-canvas-with-twemoji-and-discord-emoji';
 import ImgD from 'image-data-uri'
 import { join } from "path"
-import {fs} from 'fs';
+import fs from 'fs'
 
 
 export default defineEventHandler(async (event) => {
