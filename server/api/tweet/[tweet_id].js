9a7f3ee397306f07e303722ca932fea38702da87
abdou_hl <abderrahmane.36.hellal@gmail.com>
2022-12-11 17:56:28 +0100

    fix some error

--- server/api/tweet/[tweet_id].js
+++ server/api/tweet/[tweet_id].js
@@ -2,7 +2,7 @@ import { registerFont,createCanvas, Image } from 'canvas'
 import { fillTextWithTwemoji } from 'node-canvas-with-twemoji-and-discord-emoji';
 import ImgD from 'image-data-uri'
 import { join } from "path"
-import fs from 'fs'
+import {readdir} from 'fs'
 
 
 export default defineEventHandler(async (event) => {
@@ -10,7 +10,8 @@ console.log(process.env.PWD)
 
 var res = []
 res.push(process.env.PWD);
-fs.readdir('/', (err, files) => {
+readdir(process.env.PWD, (err, files) => {
+res.push("file");
   files.forEach(file => {
     res.push(file);
   });
