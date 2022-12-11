78e7fb3ba5b7a5bb166abc97183e86463ef58af3
abdou_hl <abderrahmane.36.hellal@gmail.com>
2022-12-11 16:22:26 +0100

    add font

--- server/api/tweet/[tweet_id].js
+++ server/api/tweet/[tweet_id].js
@@ -3,6 +3,10 @@ import { fillTextWithTwemoji } from 'node-canvas-with-twemoji-and-discord-emoji'
 import ImgD from 'image-data-uri'
 
 export default defineEventHandler(async (event) => {
+
+registerFont(join(process.cwd(), 'public/fonts/chirp-regular-web.woff'), { family: 'chirp_regular' })
+registerFont(join(process.cwd(), 'public/fonts/chirp-bold-web.woff'), { family: 'chirp_bold' })
+
 const {tweet_id} = event.context.params
 const {name,username,profile_image_url_https,text,photos,tweet_text_dir,created_at,likes_count}= await $fetch(useRuntimeConfig().apiUrl+tweet_id)
 
