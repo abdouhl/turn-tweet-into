bcb1d11a6e04be7e29aa31cee260ecadfed230b5
abdou_hl <abderrahmane.36.hellal@gmail.com>
2022-12-12 10:43:09 +0100

    fix

--- server/api/tweet/[tweet_id].js
+++ server/api/tweet/[tweet_id].js
@@ -3,11 +3,11 @@ import { fillTextWithTwemoji } from 'node-canvas-with-twemoji-and-discord-emoji'
 import ImgD from 'image-data-uri'
 import { join } from "path"
 import {readdirSync} from 'fs'
-import { useStorage } from '@vueuse'
+
 
 export default defineEventHandler(async (event) => {
 
-const data = await useStorage().getItem(`assets/fonts/chirp-extended-heavy-web.woff`)
+
 
 console.log(process.env.PWD)
 const {tweet_id} = event.context.params
