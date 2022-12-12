f9afcdfef87f0ffa82487ac1c3c2a952a9c26e9f
abdou_hl <abderrahmane.36.hellal@gmail.com>
2022-12-12 09:08:06 +0100

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 // yarn build  .nuxt/dist yarn install
-if (
+/*if (
   process.env.LD_LIBRARY_PATH == null ||
   !process.env.LD_LIBRARY_PATH.includes(
     `${process.env.PWD}/node_modules/canvas/build/Release:`,
@@ -9,7 +9,7 @@ if (
   process.env.LD_LIBRARY_PATH = `${
     process.env.PWD
   }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
-}
+}*/
 export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss"],
   runtimeConfig: {
