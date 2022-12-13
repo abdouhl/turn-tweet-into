03393bc4c16d54b15107d7b5501cf4c55dc9f0b3
abdou_hl <abderrahmane.36.hellal@gmail.com>
2022-12-13 17:24:07 +0100

    fix image dim

--- server/api/c_i/[tweet_id].js
+++ server/api/c_i/[tweet_id].js
@@ -16,12 +16,12 @@ ctx.fillRect(0, 0, canvas.width, canvas.height);
 //w_t
 const w_t_image = new Image();
 w_t_image.onload = () => ctx.drawImage(w_t_image, 0, 0,w_t_image.width,w_t_image.height,20,20,370,370)
-const w_t_imageData = await ImgD.encodeFromURL("https://rlv.zazzle.com/svc/view?pid=235293855806478511&realview=113562383382757001&max_dim=1024&at=238414036962221940&t_image1_url=https://www.turntweetinto.com/api/w_t/"+tweet_id)
+const w_t_imageData = await ImgD.encodeFromURL("https://rlv.zazzle.com/svc/view?pid=235293855806478511&realview=113562383382757001&max_dim=1080&at=238414036962221940&t_image1_url=https://www.turntweetinto.com/api/w_t/"+tweet_id)
 w_t_image.src = w_t_imageData
 //b_t
 const b_t_image = new Image();
 b_t_image.onload = () => ctx.drawImage(b_t_image, 0, 0,b_t_image.width,b_t_image.height,410,20,370,370)
-const b_t_imageData = await ImgD.encodeFromURL('https://rlv.zazzle.com/svc/view?pid=235293855806478511&realview=113562383382757001&style=hanes_mens_crew_darktshirt_5250&size=a_s&color=black&max_dim=1028&at=238414036962221940&t_image1_url=https://www.turntweetinto.com/api/b_t/'+tweet_id)
+const b_t_imageData = await ImgD.encodeFromURL('https://rlv.zazzle.com/svc/view?pid=235293855806478511&realview=113562383382757001&style=hanes_mens_crew_darktshirt_5250&size=a_s&color=black&max_dim=1080&at=238414036962221940&t_image1_url=https://www.turntweetinto.com/api/b_t/'+tweet_id)
 b_t_image.src = b_t_imageData
 
 ctx.fillStyle = "white";
