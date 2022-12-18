0d20e5714f2438ac68bd7ddc0260b158156230f9
abdou_hl <abderrahmane.36.hellal@gmail.com>
2022-12-18 20:44:52 +0100

    add rss for tshirt

--- server/routes/rss/[author_username].js
+++ server/routes/rss/[author_username].js
@@ -14,6 +14,15 @@ const feed = new Feed({
   language: "en"
 });
 
+for(var tweet_id of author.tweets) {
+  var tweet= await $fetch(useRuntimeConfig().apiUrl+tweet_id)
+  feed.addItem({
+    title: tweet.text.join(),
+    link: "https://www.turntweetinto.com/clothes/"+tweet_id+"?type=t_shirt",
+    description: author.name+" Tweet: "+tweet.text.join(),
+    image:"https://rlv.zazzle.com/svc/view?pid=235293855806478511&realview=113562383382757001&max_dim=1080&at=238414036962221940&t_image1_url=https://www.turntweetinto.com/api/w_t/"+tweet_id+"-true-true",
+  });
+}
 
 
   return  feed.rss2()
