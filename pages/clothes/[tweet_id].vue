25108817e703dc78d52e5c08edbdfb89d0b5e832
abdou_hl <abderrahmane.36.hellal@gmail.com>
2022-12-19 20:51:55 +0100

    add spinner for image loading

--- pages/clothes/[tweet_id].vue
+++ pages/clothes/[tweet_id].vue
@@ -23,6 +23,8 @@ const colorActiveBtnClasses = reactive(["color-choose","w-8","h-8","md:w-12","md
 const colorDESActiveBtnClasses = reactive(["color-choose","cursor-pointer","w-8","h-8","md:w-12","md:h-12","rounded-md","hover:ring-offset-stone-200","hover:ring-offset-4","hover:ring-2","hover:ring-white"])
 
 
+const imgloading = reactive({"state":true})
+
 const typeActiveBtnClasses = reactive(["text-xs","md:text-xl","type-choose","border","md:border-2","border-black","rounded-md","rounded-md","ring-offset-stone-200","ring-offset-2","md:ring-offset-4","ring-2","p-1","md:p-2"])
 const typeDESActiveBtnClasses = reactive(["text-xs","md:text-xl","type-choose","cursor-pointer","border","md:border-2","border-black","rounded-md","hover:ring-offset-stone-200","hover:ring-offset-2","md:hover:ring-offset-4","hover:ring-2","hover:ring-white","p-1","md:p-2"])
 const links = {
@@ -78,9 +80,13 @@ onMounted(() => {
 
 })
 
+function makeImgDone(event) {
+    imgloading.state=false
+		console.log('done')
+}
 function changeUrl(event) {
   if (event) {
-    
+    imgloading.state=true
     image.type = event.target.getAttribute('data-type')
     image.w_url = event.target.getAttribute('data-url-white')
     image.b_url = event.target.getAttribute('data-url-black')
@@ -113,6 +119,7 @@ function changeUrl(event) {
 
 function changeColor(event) {
   if (event) {
+  	imgloading.state=true
     image.color = event.target.getAttribute('data-color')
   }
 }
@@ -120,6 +127,7 @@ function changeColor(event) {
 
 //event.target.getAttribute('data-url-'+image.color)
 const image_url = computed(() => {
+		imgloading.state=true
     if(image.color=='white'){
     return image.w_url + 'https://www.turntweetinto.com/api/w_t/'+tweet_id+'-'+cond.show_media+'-'+cond.show_info
     }else if(image.color=='black'){
@@ -183,7 +191,19 @@ watch(tweet_url, (new_tweet_url) => {
 
 
     <div class="flex flex-col md:flex-row justify-items-center px-2 md:px-8 md:h-4/6 ">
-    <div class=" h-full w-fit justify-self-center"><img id="img"  class="rounded-lg border md:border-2 border-black object-contain h-full w-fit aspect-square" :src="image_url" /></div>
+    <div class="relative h-full w-fit justify-self-center ">
+    <nuxt-img  @load="makeImgDone"  id="img"  class="rounded-lg border md:border-2 border-black object-contain h-full w-fit aspect-square " :src="image_url" />
+    <div v-if="imgloading.state"  class="absolute top-0 left-0 rounded-lg border md:border-2 border-black w-full h-full grid content-center justify-center aspect-square bg-stone-100">
+<div role="status" class="w-fit" >
+    <svg class="inline mr-2 w-16 h-16 text-stone-100 animate-spin  fill-black  mx-auto" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+    </svg>
+    <span class="sr-only">Loading...</span>
+</div>
+</div>
+    
+    </div>
     <div class="w-full md:w-fit flex flex-col pl-4 md:ml-8 grow h-full justify-between">
       
       <div >
