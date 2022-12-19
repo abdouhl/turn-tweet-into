6b3421d43e07904eca1c852468348ed95166e430
abdou_hl <abderrahmane.36.hellal@gmail.com>
2022-12-19 16:18:09 +0100

    add RSS

--- server/routes/rss/[author_username].js
+++ server/routes/rss/[author_username].js
@@ -1,4 +1,5 @@
-import { Feed } from "feed";
+import RSS from "rss";
+
 
 export default defineEventHandler(async (event) => {
 
@@ -6,24 +7,27 @@ const {author_username} = event.context.params
 
 const author= await $fetch(useRuntimeConfig().apiUrl+"rss/"+author_username)
  
-const feed = new Feed({
+
+var feed = new RSS({
   title: author.name+" Tweets",
   description: "the latest "+author.name+" Tweets",
   id: "http://example.com/",
   link: "https://www.turntweetinto.com/authors/tweets/"+author_username,
   language: "en"
 });
-
+ 
 for(var tweet of author.tweets) {
-  feed.addItem({
+  feed.item({
     title: tweet.text,
-    link: "https://www.turntweetinto.com/clothes/"+tweet.id+"?type=t_shirt",
+    url: "https://www.turntweetinto.com/clothes/"+tweet.id+"?type=t_shirt",
     description: author.name+" Tweet: "+tweet.text,
-    image:"https://rlv.zazzle.com/svc/view?pid=235293855806478511&realview=113562383382757001&max_dim=1080&at=238414036962221940&t_image1_url=https://www.turntweetinto.com/api/w_t/"+tweet.id+"-true-true",
-  });
+    enclosure: {url:"https://rlv.zazzle.com/svc/view?pid=235293855806478511&realview=113562383382757001&max_dim=1080&at=238414036962221940&t_image1_url=https://www.turntweetinto.com/api/w_t/"+tweet.id+"-true-true"}, // optional enclosure
+});
 }
 
+var xml = feed.xml();
+ 
 
-  return  feed.rss2()
+  return  xml
 })
 
