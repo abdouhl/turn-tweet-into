66e1a594e23191fc495fca85b8fbf63073afb708
abdou_hl <abderrahmane.36.hellal@gmail.com>
2022-12-20 22:19:51 +0100

    add home

--- pages/clothes/index.vue
+++ pages/clothes/index.vue
@@ -28,10 +28,10 @@ const type_title = computed(() => {
 </script>
 
 <template>
-<section class="w-screen h-screen content-center  pb-4 md:pb-0" >
-<div class="flex md:flex-row flex-col w-full md:h-full  px-1 md:px-8 md:items-center">
-<h1 style="font-family: 'Lobster', cursive;" class="align-middle h-fit text-center md:text-start text-3xl my-8 md:my-2 font-bold hover:text-blue-500" ><NuxtLink  to="/">Turn Tweet Into </NuxtLink><span class="text-blue-500">{{type_title}}</span></h1>
-<div class="grow flex flex-row justify-items-center justify-center py-2 md:py-4" >
+<section class="w-screen h-screen   pb-4 md:pb-0" >
+<div class="flex  flex-col w-full h-full content-center justify-center  px-1 md:px-8 md:items-center">
+<h1 style="font-family: 'Lobster', cursive;" class="w-full h-fit text-center text-3xl my-8 md:my-2 font-bold hover:text-blue-500" ><NuxtLink  to="/">Turn Tweet Into </NuxtLink><span class="text-blue-500">{{type_title}}</span></h1>
+<div class="w-full flex flex-row justify-items-center justify-center py-2 md:py-4" >
   <input class="w-full md:w-6/12 lg:w-5/12 placeholder-gray-400 focus:placeholder-transparent text-xs md:text-xl text-gray-500 pl-1 md:pl-4  md:py-2 rounded-lg bg-stone-100  focus:outline-none" v-model="tweet_url.url" placeholder="Enter Tweet URL here">
   <NuxtLink class="self-stretch mx-1 md:mx-2  p-2 border border-black  rounded-lg bg-stone-100 text-gray-500 text-xs md:text-xl font-bold" :to="next_tweet">Submit</NuxtLink>
 </div>
--- pages/index.vue
+++ pages/index.vue
@@ -15,25 +15,58 @@ next_tweet = computed(() => {
 
 <template>
 <section class="w-screen  pb-4 md:pb-0" >
-<div class="flex md:flex-row flex-col w-full px-1 md:px-8 md:items-center">
-<h1 style="font-family: 'Lobster', cursive;" class="align-middle h-fit text-center md:text-start text-3xl my-2 font-bold hover:text-blue-500" ><NuxtLink  to="/">Turn Tweet Into</NuxtLink></h1>
-<div class="grow flex flex-row justify-items-center justify-center py-2 md:py-4" >
-  <input class="w-full md:w-6/12 lg:w-5/12 placeholder-gray-400 focus:placeholder-transparent text-xs md:text-xl text-gray-500 pl-1 md:pl-4  md:py-2 rounded-lg bg-stone-100  focus:outline-none" v-model="tweet_url.url" placeholder="Enter Tweet URL here">
-  <NuxtLink class="self-stretch mx-1 md:mx-2  p-2 border border-black  rounded-lg bg-stone-100 text-gray-500 text-xs md:text-xl font-bold" :to="next_tweet">Submit</NuxtLink>
-</div>
+<div class="w-full px-1 md:px-8 ">
+<h1 style="font-family: 'Lobster', cursive;" class="text-center text-3xl my-2 font-bold hover:text-blue-500" ><NuxtLink  to="/">Turn Tweet Into</NuxtLink></h1>
+
+
 </div>
 </section>
 
-<section class="w-screen pb-4 px-1 md:px-8" >
-<div class="w-48 rounded-lg bg-stone-100 divide-y  px-1 md:px-2">
+<section class="w-screen grid mx-auto  grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 md:gap-8 mt-2 md:mt-8 px-2 md:px-24 lg:px-36" >
+
+<!--Clothing-->
+<div class=" rounded-lg bg-stone-100 divide-y  px-1 md:px-2">
 <h2 class="text-xl text-center py-1 " style="font-family: Lobster, cursive;">Clothing</h2>
 <ul class="p-1 md:p-2">
-<li><NuxtLink class="hover:text-blue-500" to="/clothes?type=t_shirt">Tshirt</NuxtLink></li>
+<li><NuxtLink class="hover:text-blue-500" to="/clothes?type=t_shirt">T-Shirt</NuxtLink></li>
 <li><NuxtLink class="hover:text-blue-500" to="/clothes?type=s_shirt">Sweatshirt</NuxtLink></li>
 <li><NuxtLink class="hover:text-blue-500" to="/clothes?type=hoodie">Hoodie</NuxtLink></li>
+<li><NuxtLink class="line-through" >Hat</NuxtLink></li>
 </ul>
+</div>
+
+<!--Accessories-->
+<div class="rounded-lg bg-stone-100 divide-y  px-1 md:px-2">
+<h2 class="text-xl text-center py-1 " style="font-family: Lobster, cursive;">Accessories</h2>
+<ul class="p-1 md:p-2">
+<li><NuxtLink class="hover:text-blue-500" to="/clothes?type=t_shirt">Tote Bag</NuxtLink></li>
+<li><NuxtLink class="line-through" >Face Mask</NuxtLink></li>
+<li><NuxtLink class="line-through" >Water bottle</NuxtLink></li>
+<li><NuxtLink class="line-through" >Travel mug</NuxtLink></li>
+<li><NuxtLink class="line-through" >Zipper pouche</NuxtLink></li>
+<li><NuxtLink class="line-through" >Toiletry Bag</NuxtLink></li>
+</ul>
+</div>
+
+<!--Images-->
+<div class="rounded-lg bg-stone-100 divide-y  px-1 md:px-2">
+<h2 class="text-xl text-center py-1 " style="font-family: Lobster, cursive;">Images</h2>
+<ul class="p-1 md:p-2">
+<li><NuxtLink class="line-through" >Post</NuxtLink></li>
+<li><NuxtLink class="line-through" >Png</NuxtLink></li>
+
+</ul>
+</div>
 
 
+<!--Videos-->
+<div class="rounded-lg bg-stone-100 divide-y  px-1 md:px-2">
+<h2 class="text-xl text-center py-1 " style="font-family: Lobster, cursive;">Videos</h2>
+<ul class="p-1 md:p-2">
+<li><NuxtLink class="line-through" >Instagram Reel</NuxtLink></li>
+<li><NuxtLink class="line-through" >Tiktok Video</NuxtLink></li>
+
+</ul>
 </div>
 
 </section>
